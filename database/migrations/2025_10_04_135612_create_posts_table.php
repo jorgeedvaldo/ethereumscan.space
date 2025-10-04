1f8b9f768aadf5764859341f6cbfca0e98f7e719
jorgeedvaldo <edvaldojorge1996@gmail.com>
2025-10-04 15:40:06 +0100

    Add columns to posts table for title, content, image, status, and user relationship

--- database/migrations/2025_10_04_135612_create_posts_table.php
+++ database/migrations/2025_10_04_135612_create_posts_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->string('image')->nullable();
+            $table->enum('status', ['draft', 'published'])->default('draft');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
