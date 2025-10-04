e7c0e3d641a366393183354cc85fb3d0907d79ac
jorgeedvaldo <edvaldojorge1996@gmail.com>
2025-10-04 22:44:28 +0100

    Add favicon to the application

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon.png') }}">
     <title>@yield('title') | {{env('APP_NAME')}}</title>
     @yield('style')
 </head>
